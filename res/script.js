1e7947d1ee0db544735e4bcc8505b410896ea273
Chitranjan Maurya <140469446+ChitranjanMaurya@users.noreply.github.com>
2024-01-25 09:06:40 +0530

    Update script.js

--- res/script.js
+++ res/script.js
@@ -1,6 +1,6 @@
 function add() {
-    let input1 = parseInt(document.getElementById('input1').value);
-    let input2 = parseInt(document.getElementById('input2').value);
+    let input1 = parseFloat(document.getElementById('input1').value);
+    let input2 = parseFloat(document.getElementById('input2').value);
     let output;
     output = input1 + input2;
     document.getElementById('output').innerHTML = output;
